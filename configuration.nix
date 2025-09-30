4bf963e9aef0214f0add10037d8615933513a5f0
Josh Kasten <jkasten@gmail.com>
2025-09-30 00:19:58 -0400

    current 7700x config with sway

---
+++ configuration.nix
@@ -0,0 +1,180 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+let 
+  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "kasten_nixos_7700x"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  networking.interfaces.enp16s0.wakeOnLan.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.kasten = {
+    isNormalUser = true;
+    description = "kasten";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+
+    vim
+    htop
+    smem
+    ethtool
+
+    wl-clipboard # cp and paste for Sway WM
+    mako # Sway notification system
+
+    firefox
+    steam
+    unstable.heroic
+    discord
+
+    # NOTE: Gamescope must match the wayland version. Otherwise you get an error like this:
+    # [gamescope] [Error] wlserver: [xwayland/server.c:269] Xwayland startup failed, not setting up xwm
+    gamescope
+    gamemode
+
+    radeontop
+    vulkan-loader
+    vulkan-tools
+    vulkan-validation-layers
+
+    pavucontrol # PulseAudio Volume Control
+    pamixer # Command-line mixer for PulseAudio
+    bluez # Bluetooth support
+    bluez-tools # Bluetooth tools
+    blueman # GUI
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+  ];
+
+  #programs.gamescope.enable = true;
+  #programs.gamemode.enable = true;
+
+
+  hardware.pulseaudio.enable = false; # Use Pipewire, the modern sound subsystem
+
+  security.rtkit.enable = true; # Enable RealtimeKit for audio purposes
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # Uncomment the following line if you want to use JACK applications
+    # jack.enable = true;
+  };
+
+  services.gnome.gnome-keyring.enable = true;
+
+  # Sway Window Manager
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+
+    extraPackages = with pkgs; [
+       brightnessctl
+       foot
+       grim
+       pulseaudio
+       swayidle
+       swaylock
+       wmenu
+
+       # These two are for larger corsors, easier to see on a 4k screen
+       # Make sure to all this line in your ~/.config/sway/config:
+       #  seat "*" xcursor_theme Adwaita 24
+       adwaita-icon-theme # mouse cursor and icons
+       gnome-themes-extra # dark adwaita theme
+    ];
+  };
+
+  # KASTEN: TODO: I think this need to be in HomeManger
+  programs.foot.settings = {
+     main = {
+        dpi-aware = "yes";
+        font-size-adjustment = "20%";
+     };
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+}
+
