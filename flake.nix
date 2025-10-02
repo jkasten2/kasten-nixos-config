09f7f6742c6e698aaf7afa12f0da96496eccaa89
Josh Kasten <jkasten@gmail.com>
2025-10-01 23:34:17 -0400

    chore: rm extra keys and substituers
    Tested with nixos-rebuilt switch and ensure they were not in the store with nix config show.

--- flake.nix
+++ flake.nix
@@ -13,15 +13,6 @@
     # };
   };
 
-  nixConfig = {
-    extra-substituters = [ 
-      "https://nixpkgs-wayland.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-    ];
-  };
-
   outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations."KastenNixOS7700x" = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
