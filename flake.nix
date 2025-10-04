6ec12d92d4ea4e00a800001e38d980253956b400
Josh Kasten <jkasten@gmail.com>
2025-10-03 23:36:51 -0400

    feat: linux kernel 6.16 -> 6.17
    Switched to nixpkgs/master from unstable to get 6.17 sooner.
    Tested: Steam with The Finals and Heroic with Tiny Tina's Wonderland.
    
    Version changes:
    [U.]  #1  initrd-linux                   6.16.9 -> 6.17
    [U.]  #2  linux                          6.16.9, 6.16.9-modules x2, 6.16.9-modules-shrunk -> 6.17, 6.17-modules x2, 6.17-modules-shrunk
    [U.]  #3  nixos-system-KastenNixOS7700x  25.11.20251002.7df7ff7 -> 25.11.20251003.ac9f0e2
    [C.]  #4  udev-rules                     <none> x2 -> <none>
    Added packages:
    [A.]  #1  unit-filter-chain.service  <none>
    Removed packages:
    [R.]  #1  extra-utils    <none>
    [R.]  #2  initrd-fsinfo  <none>
    [R.]  #3  keymap         <none>
    [R.]  #4  link-units     <none>
    [R.]  #5  stage          1-init.sh
    Closure size: 1507 -> 1502 (51 paths added, 56 paths removed, delta -5, disk usage -24.0MiB).

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/master";
 
     nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
     nixpkgs-wayland.inputs.nixpkgs.follows = "nixpkgs";
